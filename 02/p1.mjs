ae92b8bb9049e9b6b5dbcb0967184e80930fffe4
Jack Lewis <jack@jacklew.is>
2023-12-02 00:48:21 -0500

    Finish day 2

--- 02/p1.mjs
+++ 02/p1.mjs
@@ -4,9 +4,51 @@ const path = process.argv.slice(-1)[0]
 const file = fs.readFileSync(path, 'utf-8')
 const lines = file.split('\n')
 
+let possibleGameIds = []
+
 for (const line of lines) {
     if (line.trim() === "") {
         break
     }
+
+    const gameId = line.match(/(\d+):/)[1]
+
+    let rest = line.split(':')[1].trim()
+    let games = rest.split(';').map(s => s.trim())
+
+    let possible = true;
+
+    for (const game of games) {
+        console.log(`Game ID ${gameId}: ${game}`)
+
+        let r = game.match(/(\d+) red/)
+        let g = game.match(/(\d+) green/)
+        let b = game.match(/(\d+) blue/)
+
+        r = r && r.length ? r[1] : 0
+        g = g && g.length ? g[1] : 0
+        b = b && b.length ? b[1] : 0
+
+        console.log(`Red: ${r} Green: ${g} Blue: ${b}`)
+
+        if (r > 12 || g > 13 || b > 14) {
+            possible = false;
+            break
+        }
+    }
+
+    if (possible) {
+        possibleGameIds.push(gameId)
+    }
 }
+
+console.log(possibleGameIds)
+
+let sum = 0
+
+for (let gameId of possibleGameIds) {
+    sum += parseInt(gameId)
+}
+
+console.log(sum)
 
--- 02/p2.mjs
+++ 02/p2.mjs
@@ -4,9 +4,50 @@ const path = process.argv.slice(-1)[0]
 const file = fs.readFileSync(path, 'utf-8')
 const lines = file.split('\n')
 
+let cubePowers = []
+
 for (const line of lines) {
     if (line.trim() === "") {
         break
     }
+
+    const gameId = line.match(/(\d+):/)[1]
+
+    let rest = line.split(':')[1].trim()
+    let rounds = rest.split(';').map(s => s.trim())
+
+    let possible = true;
+
+    let maxR = 0;
+    let maxG = 0;
+    let maxB = 0;
+
+    for (const round of rounds) {
+        console.log(`Game ID ${gameId}: ${round}`)
+
+        let r = round.match(/(\d+) red/)
+        let g = round.match(/(\d+) green/)
+        let b = round.match(/(\d+) blue/)
+
+        r = r && r.length ? r[1] : 0
+        g = g && g.length ? g[1] : 0
+        b = b && b.length ? b[1] : 0
+
+        console.log(`Red: ${r} Green: ${g} Blue: ${b}`)
+
+        maxR = Math.max(maxR, r)
+        maxG = Math.max(maxG, g)
+        maxB = Math.max(maxB, b)
+    }
+
+    cubePowers.push(maxR * maxG * maxB)
 }
+
+let sum = 0
+
+for (let cubePower of cubePowers) {
+    sum += cubePower
+}
+
+console.log(`Sum of cube powers: ${sum}`)
 
